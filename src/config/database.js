315f5be1aabe47ad39bf41d59fef9e4d6f6c3970
melememe <130522472+melememe@users.noreply.github.com>
2023-07-22 08:23:41 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,17 @@
 import Sequelize from 'sequelize'
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-const hostname = process.env.HOSTNAME || '127.0.0.1'
-const username = process.env.BDUSER || 'postgres'
-const password = process.env.PASSWORD || '123'
-const database = process.env.DATABASE || 'alumnos'
-const port = process.env.DBPORT || 5432
-const dialect = process.env.DIALECT || 'postgres'
+const hostname = '127.0.0.1'
+const username = 'postgres'
+const password = '123'
+const database = 'personas'
+const port = 5432
+const dialect = 'postgres'
 
 const sequelize = new Sequelize(database, username, password, {
     host: hostname,
     port,
     dialect: dialect,
     operatorAliases: false,
-    ssl: {rejectUnauthorized: false},
     pool: {
         max: 10,
         min: 0,
