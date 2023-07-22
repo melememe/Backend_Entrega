6cd6ef9278f2ef240114daea8d578206c120b488
melememe <130522472+melememe@users.noreply.github.com>
2023-07-22 08:19:30 -0500

    Update personaController.js

--- src/controllers/personaController.js
+++ src/controllers/personaController.js
@@ -1,46 +1,51 @@
-import PersonaRepository from "../repository/personaRepository.js";
+import PersonasRepository from "../repository/personaRepository.js";
 
-const findAll = async (req, res) => {
-    const result = await PersonaRepository.findAll();
+const findAll = (req, res) => {
+    const result = PersonasRepository.findAll();
 
-    return sendResponse(result, res);
-}
-
-const findOne = async (req, res) => {
-    const id = req.params.id;
-    const result = await PersonaRepository.findOne(id);
-
-    return sendResponse(result, res);
-}
-
-const create = async (req, res) => {
-    
-    const result = await PersonaRepository.create(req.body);
-
-    return sendResponse(result, res);
-}
-
-const update = async (req,res) => {
-    const result = await PersonaRepository.update(req.body)
-
-    return sendResponse(result, res);
-}
-
-const remove = async (req, res) => {
-
-    const id = req.params.id;
-
-    const result = await PersonaRepository.remove(id)
-
-    return sendResponse(result, res);
-}
-
-const sendResponse = (result, res) => {
     if (result)
         return res.status(200).json(result);
     else
         return res.status(500).json({ message: 'Ha ocurrido un error'})
-} 
+}
+
+const findOne = (req, res) => {
+    const id = req.params.id;
+    const result = PersonasRepository.findOne(id);
+
+    if (result)
+        return res.status(200).json(result);
+    else
+        return res.status(500).json({ message: 'Ha ocurrido un error'})
+}
+
+const create = (req, res) => {
+    
+    const result = PersonasRepository.create(req.body);
+
+    if (result)
+        return res.status(200).json(result);
+    else
+        return res.status(500).json({ message: 'Ha ocurrido un error'})
+}
+
+const update = (req,res) => {
+    PersonasRepository.update(req.body)
+
+    return res.status(200).json(req.body);
+}
+
+const remove = (req, res) => {
+
+    const id = req.params.id;
+
+    const result = PersonasRepository.remove(id)
+
+    if (result)
+        return res.status(200).json(result);
+    else
+        return res.status(500).json({ message: 'Ha ocurrido un error'})
+}
 
 const PersonaController = { findAll, create, findOne, update, remove }
 
